59770cc6e9c60398b9dcf4f90d02c0bb424e29a9
MasterKenway <40176260+MasterKenway@users.noreply.github.com>
2023-02-26 19:01:05 +0800

    [WIP] Refactor Benchmark (#2)
    * refactor: first version
    
    * feat: add more dubbo origin bench cases
    feat: add pixiu bench cases
    
    * feat: add more pixiu to dubbo cases
    
    * feat: add triple case
    
    * fix: triple test cases
    fix: dubbo test
    
    * feat: add grpc cases
    
    * feat: add grpc cases
    
    * chore: remove logs
    
    ---------
    
    Co-authored-by: Edward Kenway <caijm15@chinaunicom.cn>

--- script/wrk_post.lua
+++
@@ -1,5 +0,0 @@
--- example HTTP POST script which demonstrates setting the
--- HTTP method, body, and adding a header
-wrk.method = "POST"
-wrk.body   = '{"id":"0003","code":3,"name":"dubbogo","age":99}'
-wrk.headers["Content-Type"] = "application/json"
